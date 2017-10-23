22b423605862d8bd165e4730977f0ebf0a34e21f
Matiss Janis Aboltins <matiss@mja.lv>
2017-10-23 20:25:52 +0300

    Andris pinger

---
+++ pinger-andris.js
@@ -0,0 +1,175 @@
+'use strict';
+
+require('dotenv').config();
+
+const Q       = require('q');
+const fs      = require('fs');
+const Bugsnag = require('bugsnag');
+const Mysql   = require('mysql');
+const mailgun = require('mailgun-js')({
+  apiKey: process.env.MAILGUN_API_KEY, 
+  domain: process.env.MAILGUN_DOMAIN,
+});
+const Handlebars = require('handlebars');
+const numeral = require('numeral');
+
+const fileName = 'previous-date-andris.txt';
+
+Bugsnag.register('76d5f4207c779acf8eea5ae606a25ca9');
+
+function nl2br(str, is_xhtml) {
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+}
+
+// Read the date of the last call
+Q.fcall(() => {
+  const deferred = Q.defer();
+  const fsConstants = fs.constants || fs;
+
+  fs.access(fileName, fsConstants.R_OK | fsConstants.W_OK, (err) => {
+    if (err) {
+      const currentDate = (new Date()).toISOString();
+
+      fs.writeFile(fileName, currentDate, (err) => {
+        if (err) {
+          deferred.reject(err);
+          return;
+        }
+
+        deferred.resolve(currentDate);
+      });
+    }
+
+    fs.readFile(fileName, 'utf8', (err, data) => {
+      if (err) {
+        deferred.reject(err);
+        return;
+      }
+
+      deferred.resolve(data);
+    });
+  });
+
+  return deferred.promise;
+})
+
+// Load the properties since last call
+.then((date) => {
+  const deferred = Q.defer();
+
+  const connection = Mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE,
+    timezone : 'Z',
+    typeCast : true,
+  });
+
+  connection.connect();
+
+  const query = `
+    SELECT * 
+    FROM properties 
+    WHERE created_at > ? 
+      AND category = "garage"
+      AND type = "sell"
+      AND price >= 10000
+      AND ST_Contains(ST_GeomFromText('POLYGON((57.00747 24.05457, 56.9856 24.0015, 56.95791 23.9916, 56.9431 23.98727, 56.92549 23.98384, 56.91612 24.00169, 56.90338 24.05937, 56.89625 24.09714, 56.897 24.12872, 56.90563 24.15688, 56.91388 24.17816, 56.92774 24.20563, 56.94643 24.22755, 56.9622 24.23447, 56.98504 24.21554, 57.00583 24.15522, 57.00747 24.05457))'), point(lat, lng))
+    ORDER BY created_at
+  `;
+
+  connection.query(query, [date], (error, results) => {
+    connection.destroy();
+
+    if (error) {
+      deferred.reject(error);
+      return;
+    }
+
+    deferred.resolve(results);
+  });
+
+  return deferred.promise;
+})
+
+// Write the date back in the file
+.then((results) => {
+  if (results.length === 0) {
+    return results;
+  }
+
+  const deferred = Q.defer();
+  const lastDate = (new Date(results[results.length - 1].created_at)).toISOString();
+
+  fs.writeFile(fileName, lastDate, (err) => {
+    if (err) {
+      deferred.reject(err);
+      return;
+    }
+
+    deferred.resolve(results);
+  });
+
+  return deferred.promise;
+})
+
+// Send notifications
+.then((results) => {
+  if (results.length === 0) {
+    return;
+  }
+
+  return Q.all(results.map((result) => {
+    const deferred = Q.defer();
+
+    fs.readFile('email.html', 'utf8', (err, content) => {
+      if (err) {
+        deferred.reject(err);
+        return;
+      }
+
+      result.content = nl2br((result.content || '').toString('utf8').replace(/(<([^>]+)>)/ig, ""));
+
+      if (result.images) {
+        result.images = JSON.parse(result.images);
+      }
+
+      result.url = `https://view.brokalys.com/?link=${encodeURIComponent(result.url)}`;
+      result.price = numeral(result.price).format('0,0 €');
+
+      const template = Handlebars.compile(content);
+      const html = template(result);
+
+      var data = {
+        from: 'Brokalys <noreply@brokalys.com>',
+        to: 'andris.lanka@gmail.com',
+        bcc: 'matiss@brokalys.com, kristaps@brokalys.com',
+        subject: 'Garāža: Jauns PINGER sludinājums',
+        html,
+      };
+
+      mailgun.messages().send(data, (error, body) => {
+        if (error) {
+          deferred.reject(error);
+          return;
+        }
+
+        console.log(body);
+        deferred.resolve();
+      });
+    });
+
+    return deferred.promise;
+  }));
+})
+
+.then(() => {
+  console.log('DONE. No errors.');
+})
+
+// Catch errors
+.catch((error) => {
+  console.error('error', error);
+});
---
+++ src/pinger.js
@@ -0,0 +1,5 @@
+// Loop:
+// - Get previous timestamp
+// - Get all ads matching a criteria and within timestamp
+// - Update timestamp
+// - Send email
