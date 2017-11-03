a87ab239196aa4ce9de47ddeb58e754087d65616
Matiss Janis Aboltins <matiss@mja.lv>
2017-11-03 18:17:41 +0200

    New pingers

---
+++ pinger-4.js
@@ -0,0 +1,173 @@
+'use strict';
+
+require('dotenv').config();
+
+const Q       = require('q');
+const fs      = require('fs');
+const Bugsnag = require('bugsnag');
+const Mysql   = require('mysql');
+const mailgun = require('mailgun-js')({
+  apiKey: process.env.MAILGUN_API_KEY, 
+  domain: process.env.MAILGUN_DOMAIN,
+});
+const Handlebars = require('handlebars');
+const numeral = require('numeral');
+
+const fileName = 'previous-date-1.txt';
+
+Bugsnag.register('76d5f4207c779acf8eea5ae606a25ca9');
+
+function nl2br(str, is_xhtml) {
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+}
+
+// Read the date of the last call
+Q.fcall(() => {
+  const deferred = Q.defer();
+  const fsConstants = fs.constants || fs;
+
+  fs.access(fileName, fsConstants.R_OK | fsConstants.W_OK, (err) => {
+    if (err) {
+      const currentDate = (new Date()).toISOString();
+
+      fs.writeFile(fileName, currentDate, (err) => {
+        if (err) {
+          deferred.reject(err);
+          return;
+        }
+
+        deferred.resolve(currentDate);
+      });
+    }
+
+    fs.readFile(fileName, 'utf8', (err, data) => {
+      if (err) {
+        deferred.reject(err);
+        return;
+      }
+
+      deferred.resolve(data);
+    });
+  });
+
+  return deferred.promise;
+})
+
+// Load the properties since last call
+.then((date) => {
+  const deferred = Q.defer();
+
+  const connection = Mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE,
+    timezone : 'Z',
+    typeCast : true,
+  });
+
+  connection.connect();
+
+  const query = `
+    SELECT * 
+    FROM properties 
+    WHERE created_at > ?
+      AND type = "sell"
+      AND ST_Contains(ST_GeomFromText('POLYGON((57.02877 23.47366, 56.98989 23.51349, 56.96145 23.57254, 56.95396 23.69751, 56.98241 23.89526, 57.02653 23.99963, 57.0567 24.04697, 57.11089 24.22211, 57.16002 24.29069, 57.2609 24.44408, 57.27682 24.42123, 57.29567 24.41184, 57.03317 23.44044, 57.02877 23.47366))'), point(lat, lng))
+    ORDER BY created_at
+  `;
+
+  connection.query(query, [date], (error, results) => {
+    connection.destroy();
+
+    if (error) {
+      deferred.reject(error);
+      return;
+    }
+
+    deferred.resolve(results);
+  });
+
+  return deferred.promise;
+})
+
+// Write the date back in the file
+.then((results) => {
+  if (results.length === 0) {
+    return results;
+  }
+
+  const deferred = Q.defer();
+  const lastDate = (new Date(results[results.length - 1].created_at)).toISOString();
+
+  fs.writeFile(fileName, lastDate, (err) => {
+    if (err) {
+      deferred.reject(err);
+      return;
+    }
+
+    deferred.resolve(results);
+  });
+
+  return deferred.promise;
+})
+
+// Send notifications
+.then((results) => {
+  if (results.length === 0) {
+    return;
+  }
+
+  return Q.all(results.map((result) => {
+    const deferred = Q.defer();
+
+    fs.readFile('email.html', 'utf8', (err, content) => {
+      if (err) {
+        deferred.reject(err);
+        return;
+      }
+
+      result.content = nl2br((result.content || '').toString('utf8').replace(/(<([^>]+)>)/ig, ""));
+
+      if (result.images) {
+        result.images = JSON.parse(result.images);
+      }
+
+      result.url = `https://view.brokalys.com/?link=${encodeURIComponent(result.url)}`;
+      result.price = numeral(result.price).format('0,0 €');
+
+      const template = Handlebars.compile(content);
+      const html = template(result);
+
+      var data = {
+        from: 'Brokalys <noreply@brokalys.com>',
+        to: 'janis@balticreal.lv',
+        bcc: 'matiss@brokalys.com, kristaps@brokalys.com',
+        subject: 'Saulkrasti-Ragaciems: Jauns PINGER sludinājums',
+        html,
+      };
+
+      mailgun.messages().send(data, (error, body) => {
+        if (error) {
+          deferred.reject(error);
+          return;
+        }
+
+        console.log(body);
+        deferred.resolve();
+      });
+    });
+
+    return deferred.promise;
+  }));
+})
+
+.then(() => {
+  console.log('DONE. No errors.');
+})
+
+// Catch errors
+.catch((error) => {
+  console.error('error', error);
+});
