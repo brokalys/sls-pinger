b55a4cfad3360235534f5d6a0378fdb2bf9c14c2
Matiss Janis Aboltins <matiss@mja.lv>
2021-02-10 20:49:55 +0000

    :arrow_up:  upgrade to Serverless v2

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,12 @@ const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 const CopyPlugin = require('copy-webpack-plugin');
 
-require('dotenv').config();
-
-const env = Object.entries(process.env).reduce(
-  (common, [key, value]) => ({
-    ...common,
-    [`process.env.${key}`]: JSON.stringify(value),
-  }),
-  {},
-);
-
 module.exports = {
   mode: 'development',
   entry: slsw.lib.entries,
   externals: [nodeExternals()],
   target: 'node',
-  plugins: [
-    new webpack.DefinePlugin(env),
-    new CopyPlugin({ patterns: ['src/**/*.html'] }),
-  ],
+  plugins: [new CopyPlugin({ patterns: ['src/**/*.html'] })],
   optimization: {
     minimize: true,
     minimizer: [new HtmlMinimizerPlugin()],
